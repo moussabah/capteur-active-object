be70a96159b7d3ae8cd7bfebb11329af54db696f
TuoAdama <tuoadama17@gmail.com>
2023-12-08 08:05:53 +0100

    feat: .gitignore

---
+++ com/aoc/Afficheur.java
@@ -0,0 +1,8 @@
+package com.aoc;
+
+public class Afficheur implements ObserverCapteur{
+    @Override
+    public void update(Capteur capteur) {
+
+    }
+}
---
+++ com/aoc/Canal.java
@@ -0,0 +1,9 @@
+package com.aoc;
+
+public class Canal implements ObserverCapteur {
+    @Override
+    public void update(Capteur capteur) {
+
+        
+    }
+}
