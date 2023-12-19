62a52479b226aea2eb15cc3121f0b5733fc52140
TuoAdama <tuoadama17@gmail.com>
2023-12-19 10:09:57 +0100

    feat: ajout d'observerDeCapteurAsyncs (Canaux Proxy)

--- com/aoc/CapteurImpl.java
+++ com/aoc/CapteurImpl.java
@@ -1,11 +1,17 @@
 package com.aoc;
 
+import com.aoc.proxy.ObserverDeCapteurAsync;
 import com.aoc.servent.ObserverDeCapteur;
+
+import java.util.List;
 
 public class CapteurImpl implements  Capteur{
     private AlgoDiffusion algoDiffusion;
 
-    public CapteurImpl(AlgoDiffusion algoDiffusion){
+    private List<ObserverDeCapteurAsync> observerDeCapteurAsyncs;
+
+    public CapteurImpl(List<ObserverDeCapteurAsync> observerDeCapteurAsyncs, AlgoDiffusion algoDiffusion){
+        this.observerDeCapteurAsyncs = observerDeCapteurAsyncs;
         this.algoDiffusion = algoDiffusion;
         this.algoDiffusion.configure(this);
     }
--- com/aoc/Main.java
+++ com/aoc/Main.java
@@ -1,7 +1,20 @@
 package com.aoc;
 
+import com.aoc.proxy.Canal;
+import com.aoc.proxy.ObserverDeCapteurAsync;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        AlgoDiffusion alog = new DiffusionAtomique();
+        List<ObserverDeCapteurAsync> observerDeCapteurAsyncs = new ArrayList<>();
+        observerDeCapteurAsyncs.add(new Canal());
+        observerDeCapteurAsyncs.add(new Canal());
+        observerDeCapteurAsyncs.add(new Canal());
+        observerDeCapteurAsyncs.add(new Canal());
+        Capteur c = new CapteurImpl(observerDeCapteurAsyncs, alog);
+        c.tick();
     }
 }
